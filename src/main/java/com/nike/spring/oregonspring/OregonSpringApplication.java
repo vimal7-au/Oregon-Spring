f8a70d3be7e6034f7add265090593fabeef86c88
VRadh1 <vimal.radhakrishnanmaliakkal@nike.com>
2018-05-26 07:45:13 +1000

    Initial commit

---
+++ src/main/java/com/nike/spring/oregonspring/OregonSpringApplication.java
@@ -0,0 +1,12 @@
+package com.nike.spring.oregonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OregonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OregonSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nike/spring/oregonspring/OregonSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nike.spring.oregonspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OregonSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
